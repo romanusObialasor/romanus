643ce8f48b18691d2f0ec2d676d2a6c9fcff1827
romanusObialasor <robialasor5@gmail.com>
2021-05-14 13:58:13 +0100

    javascript class

---
+++ math.js
@@ -0,0 +1,91 @@
+// /*let a = 25
+// let b = 50
+// let c = a + b
+// let c1 = a - b
+// let c2 = a/b
+// let c3 = a%b
+// alert(c)
+// alert(c1)
+// alert(c2)
+// alert(c3)
+// let num = 5+5*3
+// alert(num)*/
+
+// /*let num ="put in your first number"
+// let p = prompt(num)
+// let num2 ="put in your second number"
+// let p2 = prompt(num2) 
+// alert(Number(p) + Number(p2))*/
+
+// /*let a = 50
+// a--
+// console.log(a)*/
+
+// /*to add a number to a variable
+// let num = 10
+// num /= 5
+// console.log(num)*/
+// /*console.log(6>4)
+// console.log(6<4)
+
+// let a = 5;
+// let b = 4;
+// let c = "5";
+// console.log(a===c)
+
+// console.log(typeof a === "string")
+// let man = true
+// let woman = false
+// alert(a>b ? "correct":"wrong")
+// alert(!man ? "correct":"wrong")
+
+// let a = prompt("enter first number")
+// Number(a)
+// let b = prompt("enter second number")
+// Number(b)
+// alert(a===b ? "its equal to" : "its not equal to")*/
+
+// console.log(4>3 &&  3>4)
+// console.log(7===7 || 5<3)
+
+
+// let a = prompt("put in a number")
+// let result = Number(a)
+// b = a / 2
+// console.log(a === float() ?  "odd number" : "even number")
+
+// function sum() {
+//     let a = 10
+//     let b = 20
+//     return a+b
+// }
+
+// console.log(sum())
+ 
+// const sub = () => {
+//     let a = 26
+//     let b = 2
+
+//     return a/b
+// }
+
+// console.log(sub())
+
+
+
+// function result(){
+//     let a = 35
+//     let b = 20
+
+//     return a-b
+// }
+
+// console.log(result())
+
+// function sqrt(){
+//     let a = prompt("input a number: ")
+//     let b = Number(a) / 2
+
+//     return b
+// }
+
